69225b24f20a9a9a161e94da11a10b333049650c
Asher T Chiwashira <92381139+Tkdzw@users.noreply.github.com>
2026-02-12 22:09:11 +0200

    Initial commit backend application structure implementation. (scaffolding)

---
+++ backend/WeatherApp/WeatherApp.API/Controllers/PreferencesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApp.API.Controllers
+{
+    public class PreferencesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ backend/WeatherApp/WeatherApp.API/Controllers/WeatherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApp.API.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
